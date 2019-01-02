50832bf56239143c4ffe8858891ce895f6622af2
Stephen Wright <scw248@gmail.com>
2019-01-02 22:01:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elseif phrase == "I LOVE YOU GRANDMA!"
+  else if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else 
     return "HUH?! SPEAK UP, SONNY!"
